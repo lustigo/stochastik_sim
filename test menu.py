0a5a254b4dacd538e16d98d0bb9953d7988c71ee
wiizarrrd <patrick1933@live.de>
2016-04-18 18:57:00 +0200

    Added files via upload

--- test menu.py
+++ test menu.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 root=Tk()
 x=str(1600)
 y=str(900)
@@ -14,13 +15,15 @@ drop.config(font=('Arial',(30)),bg='white')
 drop['menu'].config(font=('calibri',(200)),bg='white')
 drop.pack(side=TOP)
 photo = PhotoImage(file="zahnrad1.png")
-photo=photo.zoom(1,1)
 label = Label(image=photo,borderwidth=0)
 label.image = photo 
 label.place(y=0,x=int(x)-200)
-print(var1.get())
-#root.mainloop()
-startbutton=Button(root,text='Start',font=('Arial',40),bg='blue',borderwidth=5)       #command=
+startbutton=Button(root,text='Start',font=('Arial',40),bg='blue',borderwidth=5)       #command=start ?! -> var1.get()
 startbutton.place(x=0,y=int(y)-100,width=int(y)-200,height=100)
-csvbutton=Button(root,text='Export CSV',font=('Arial',40),bg='green',borderwidth=5)
+csvbutton=Button(root,text='Export CSV',font=('Arial',40),bg='green',borderwidth=5)     #command=exportCSV ?!
 csvbutton.place(x=int(x)/2+50,y=int(y)-100,width=int(y)-200,height=100)
+
+
+
+
+root.mainloop()
