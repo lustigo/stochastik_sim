3c9331a20eb5bdc3174089a0d2a2528a8b25a4e8
Pat Rick <patrick1933@live.de>
2016-04-24 14:56:27 +0200

    too much importware xD

--- Start.py
+++ Start.py
@@ -1,11 +1,3 @@
-from tkinter import *
-from random import randint,shuffle
-from Marriage import Marriage
-from Atom import Atom
-from BubbleGum import BubbleGum
-from House_of_Cards import House_of_Cards
-from Lotto import Lotto
-from SecretSanta import SecretSanta
 from Ubuntu_layout import Frontend
 
 #Combines Front&Backend
